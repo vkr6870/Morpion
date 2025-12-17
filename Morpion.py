db86cbbba1b0fd1c447b64899871d139b87bd439
Owen Koehl <electrikfox13@gmail.com>
2025-12-17 14:30:19 +0100

    Add files via upload

--- Morpion.py
+++ Morpion.py
@@ -1,21 +1,40 @@
+def victoire(grille, joueur):
+    for ligne in grille:
+        if ligne == [joueur]*3:
+            return True
+
+    for col in range(3):
+        if [grille[i][col] for i in range(3)] == [joueur]*3:
+            return True
+        
+    if grille[0][0] == joueur and grille[1][1] == joueur and grille[2][2] == joueur :
+        return True
+    
+    if grille[0][2] == joueur and grille[1][1] == joueur and grille[2][0] == joueur :
+        return True
+
+    return False
+
+
 def morpion():
-    grille = [[" "]*3]*3
-    joueur = "X"
+    grille = [[" " for j in range(3)] for i in range(3)]
 
     for tour in range(9):
-        print(grille)
+        if tour % 2 == 0:
+            joueur = 'X'
+        else:
+            joueur = 'O'
+
+        for ligne in grille:
+            print(ligne)
+            
         x = int(input("ligne : "))
         y = int(input("colonne : "))
 
         grille[x][y] = joueur
 
-        if grille[0][0] == joueur:
+        if victoire(grille, joueur) == True:
             print(joueur, "a gagné")
             break
-
-        if joueur == "X":
-            joueur = "X"
-        else:
-            joueur = "O"
-
+        
 morpion()
